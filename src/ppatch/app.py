f85bfd408b792a0603c8475679aad3b964b29b0f
jingfelix <jingfelix@outlook.com>
2024-06-30 16:33:16 +0800

    feat: use cscopy to get c symbols
    Signed-off-by: jingfelix <jingfelix@outlook.com>

--- src/ppatch/app.py
+++ src/ppatch/app.py
@@ -33,4 +33,5 @@ from ppatch.commands.auto import auto
 from ppatch.commands.get import getpatches
 from ppatch.commands.help import show_settings
 from ppatch.commands.show import show
+from ppatch.commands.symbol import getsymbol_command
 from ppatch.commands.trace import trace
