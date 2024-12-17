0318e20ea4f9813473fbcfe4f7e53709f27f754d
Shuhao Shen <74161809+thefool0000@users.noreply.github.com>
2024-12-17 13:58:08 +0800

    feat: support empty file (/dev/null) (#1)
    * feat: support empty file (/dev/null)
    
    Signed-off-by: thefool0 <74161809+thefool0000@users.noreply.github.com>
    
    * deps: remove cscopy
    
    Signed-off-by: jingfelix <jingfelix@outlook.com>
    
    * fix: remove redundant deps in requirements.txt
    
    Signed-off-by: jingfelix <jingfelix@outlook.com>
    
    ---------
    
    Signed-off-by: thefool0 <74161809+thefool0000@users.noreply.github.com>
    Signed-off-by: jingfelix <jingfelix@outlook.com>
    Co-authored-by: jingfelix <jingfelix@outlook.com>

--- src/Patche/commands/apply.py
+++ src/Patche/commands/apply.py
@@ -37,29 +37,58 @@ def apply(
             old_filename = diff.header.old_path
             new_filename = diff.header.new_path
 
-            if os.path.exists(old_filename):
+            logger.debug(f"old_filename: {old_filename} new_filename: {new_filename}")
 
-                logger.info(f"Applying patch to {old_filename}...")
+            if old_filename == "/dev/null":
+                # 根据 diffes 创建新文件
+                try:
+                    assert len(diff.hunks) == 1
 
-                new_line_list = File(file_path=old_filename).line_list
-                apply_result = apply_change(
-                    diff.hunks, new_line_list, reverse=reverse, fuzz=fuzz
-                )
-                new_line_list = apply_result.new_line_list
+                    new_line_list = []
+                    for line in diff.changes:
 
-                # 检查失败数
-                for failed_hunk in apply_result.failed_hunk_list:
-                    has_failed = True
-                    logger.error(f"Failed hunk: {failed_hunk.index}")
-            else:
-                logger.error(f"{old_filename} not found!")
-                raise typer.Exit(code=1)
+                        assert line.old is None
+                        new_line_list.append(line)
 
-            # 写入文件
-            if not has_failed:
-                with open(new_filename, mode="w+", encoding="utf-8") as f:
-                    for line in new_line_list:
-                        if line.status:
+                    with open(new_filename, mode="w+", encoding="utf-8") as f:
+                        for line in new_line_list:
                             f.write(line.content + "\n")
+
+                except AssertionError:
+                    logger.error("Failed to create new file: invalid diff!")
+                    raise typer.Exit(code=1)
+
+            elif new_filename == "/dev/null":
+                # 移除 old_filename
+                if os.path.exists(old_filename):
+                    os.remove(old_filename)
+                else:
+                    logger.error(f"{old_filename} not found!")
+                    raise typer.Exit(code=1)
+            else:
+                if os.path.exists(old_filename):
+
+                    logger.info(f"Applying patch to {old_filename}...")
+
+                    new_line_list = File(file_path=old_filename).line_list
+                    apply_result = apply_change(
+                        diff.hunks, new_line_list, reverse=reverse, fuzz=fuzz
+                    )
+                    new_line_list = apply_result.new_line_list
+
+                    # 检查失败数
+                    for failed_hunk in apply_result.failed_hunk_list:
+                        has_failed = True
+                        logger.error(f"Failed hunk: {failed_hunk.index}")
+                else:
+                    logger.error(f"{old_filename} not found!")
+                    raise typer.Exit(code=1)
+
+                # 写入文件
+                if not has_failed:
+                    with open(new_filename, mode="w+", encoding="utf-8") as f:
+                        for line in new_line_list:
+                            if line.status:
+                                f.write(line.content + "\n")
 
     raise typer.Exit(code=1 if has_failed else 0)
